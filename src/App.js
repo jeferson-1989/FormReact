e607e4c488c763b4ad3a627d0f3e7ec71a13d587
jeferson-1989 <jeferson.santosn20@gmail.com>
2020-08-21 12:28:54 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+      <div>
+        <h2>Formulario de cadastro</h2>
+        </div><br/>
+
+        <label>Nome Completo<input type="text"/></label>
+        <label>CPF<input type="text"/></label>
+        <label>E-mail<input type="text"/></label>
+
+        <div>
+          <h2>Endereço</h2>
+          </div>
+
+        <label>Cep<input type="text"/></label><br/>
+        <label>Rua<input type="text"/></label><br/>
+        <label>Numero<input type="text"/></label><br/>
+        <label>Complemento<input type="text"/></label><br/>
+        <label>Bairro<input type="text"/></label><br/>
+        <label>Cidade<input type="text"/></label><br/>
+        <label>Estado<input type="text"/></label><br/>
+      </form>
+    </div>
+  );
+}
+
+export default App;
